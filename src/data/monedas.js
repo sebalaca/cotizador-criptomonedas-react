2da983d7429a17e2f27238db8a2925985879e8ae
Sebastian <seba.laca@gmail.com>
2022-10-18 12:34:05 +0200

    last commit

---
+++ src/data/monedas.js
@@ -0,0 +1,15 @@
+
+const monedas = [
+    {id: 'USD', nombre: 'Dolar Estados Unidos'},
+    {id: 'ARS', nombre: 'Peso Argentino'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'BRL', nombre: 'Real Brasileño'},
+    {id: 'CNY', nombre: 'China Renminbi'},
+    {id: 'CHF', nombre: 'Franco Suizo'},
+    {id: 'RUB', nombre: 'Rublo Ruso'},
+]
+
+export {
+    monedas
+}
